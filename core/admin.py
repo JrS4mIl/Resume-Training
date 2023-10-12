9a3d03e2424b0431b9c07c994dd6fef02c29bd69
JrSamil <samil_ysl44@hotmail.com>
2023-10-13 02:33:24 +0300

    feat: generalsetting admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from  core.models import *
+# Register your models here.
 
 # Register your models here.
+
+@admin.register(GeneralSetting)
+class GeneralSettingAdmin(admin.ModelAdmin):
+    list_display = ['id','name','paramater','created_date','update_date']
+    search_fields = ['name','paramater']
+    list_editable = ['paramater']
+
+    class Meta:
+        model = GeneralSetting
