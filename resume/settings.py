1749fb30ae6eed221b079ddbf98448c2b7ea3854
JrSamil <samil_ysl44@hotmail.com>
2023-10-12 04:54:29 +0300

    feat: template

--- resume/settings.py
+++ resume/settings.py
@@ -124,13 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL= '/static/'
+
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL='/media/'
-MEDIA_ROOT=BASE_DIR/ 'media'
-
-STATICFILES_DIRS=[
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
