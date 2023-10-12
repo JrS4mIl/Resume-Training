c3cc8d261dbb279ffb217b612e8c4dd8bdf5d981
JrSamil <samil_ysl44@hotmail.com>
2023-10-13 02:25:47 +0300

    feat: generalsetting model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class GeneralSetting(models.Model):
+    name=models.CharField(default='',max_length=100,blank=True)
+    description=models.CharField(default='',max_length=254,blank=True)
+    paramater=models.CharField(default='',max_length=254,blank=True)
+    update_date = models.DateTimeField(blank=True, auto_now=True)
+    created_date = models.DateTimeField(blank=True, auto_now_add=True)
+
+    def __str__(self):
+        return f'General Setting:{self.name}'
+
+    class Meta:
+        verbose_name='General Setting'
+        verbose_name_plural='General Settings'
+        ordering=('name',)
